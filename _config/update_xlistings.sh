ffd9a6d8a34e8f904632f92609efa693764378a3
Florian Sihler <florian.sihler@uni-ulm.de>
2024-12-27 09:24:38 +0000

    Sophisticated Documentation

--- _config/update_xlistings.sh
+++ _config/update_xlistings.sh
@@ -1,1 +1,8 @@
 wget -O ./xlistings.sty https://raw.githubusercontent.com/EagleoutIce/xlistings/refs/heads/main/xlistings.sty
+mkdir -p langs
+
+LANGUAGES=("aspectj" "bash" "cpp" "csharp" "git" "haskell" "java" "javascript" "json" "lambda_calculus" "latex" "lua" "markdown" "mips" "python" "regex" "sql" "ts" "xml" "R")
+
+for lang in "${LANGUAGES[@]}"; do
+   wget -O "./langs/$lang.cfg" "https://raw.githubusercontent.com/EagleoutIce/xlistings/refs/heads/main/langs/$lang.cfg"
+done
